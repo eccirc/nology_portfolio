6a564fde5bc99185178574e36b53db3d96641390
dagawi <dagawi@dagawi-pop.localdomain>
2022-01-20 15:01:57 +0000

    add smooth scrolling to nav links with js

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,10 +1,15 @@
-window.onload = function () {
+window.onload = () => {
+  //Get corresponding dom elements
+  //menu button
   const button = document.getElementById("menu_button");
+  //naigation bar
   const nav = document.getElementById("nav_links");
+  //the button icon
   const icon = document.getElementById("menu_icon");
+
   let toggle = false;
 
-  button.onclick = function (event) {
+  const toggleMenu = () => {
     toggle = !toggle;
 
     if (toggle) {
@@ -14,5 +19,37 @@ window.onload = function () {
       nav.classList.remove("show-menu");
       icon.src = "./assets/menu.svg";
     }
+  };
+
+  button.onclick = () => {
+    toggleMenu();
+  };
+
+  //menu buttons
+  const homeButton = document.getElementById("Nav_home");
+  const projectsButton = document.getElementById("Nav_projects");
+  const aboutButton = document.getElementById("Nav_about");
+  const contactButton = document.getElementById("Nav_contact");
+
+  const scrollToSection = (section) => {
+    const element = document.querySelector(section);
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    toggleMenu();
+  };
+
+  homeButton.onclick = () => {
+    scrollToSection("#Home");
+  };
+  projectsButton.onclick = () => {
+    scrollToSection("#Projects");
+  };
+  aboutButton.onclick = () => {
+    scrollToSection("#About");
+  };
+  contactButton.onclick = () => {
+    scrollToSection("#Contact");
   };
 };
